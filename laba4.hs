9a5bfcfe637df3e13c0264ab154dff503f5eb704
Vasiliy <sc-2005@yandex.ru>
2023-04-19 20:49:26 +0700

    add

--- laba4.hs
+++ laba4.hs
@@ -1,18 +1,24 @@
 import Data.List (sortBy)
 import Data.Function (on)
 
-type Flight = (String, String, String, String, String) -- (рейс, пункт вылета, пункт прилета, время вылета, время прилета)
-
-main = do
-  contents <- readFile "flights.txt" -- чтение данных из файла
-  let flights = map parseFlight (lines contents) -- преобразование строк в кортежи типа Flight
-  let dep = "Moscow" -- заданный пункт вылета
-  let arr = "New-York" -- заданный пункт прилета
-  let possibleFlights = filter (\(f, d, a, _, _) -> d == dep && a == arr) flights -- фильтрация списка рейсов
-  let sortedFlights = sortBy (compare `on` (\(_, _, _, depTime, arrTime) -> depTime ++ arrTime)) possibleFlights -- сортировка по времени перелета
-  print sortedFlights
+type Flight = (String, String, String, String, String)
 
 -- преобразование строки в кортеж типа Flight
 parseFlight :: String -> Flight
 parseFlight line = (f, d, a, depTime, arrTime)
   where [f, d, a, depTime, arrTime] = words line
+
+main = do
+  contents <- readFile "flights.txt" -- чтение данных из файла
+  let flights = map parseFlight (lines contents) -- преобразование строк в кортежи типа Flight
+  
+  putStrLn("Введите город вылета:")
+  dep <- getLine
+
+  putStrLn("Введите город прилета:")
+  arr <- getLine
+
+  let possibleFlights = filter (\(_, d, a, _, _) -> d == dep && a == arr) flights -- фильтрация списка рейсов
+  let sortedFlights = sortBy (compare `on` (\(_, _, _, depTime, arrTime) -> depTime ++ arrTime)) possibleFlights -- сортировка по времени перелета
+  print sortedFlights
+
--- test.hs
+++ test.hs
@@ -1,18 +1,8 @@
-map2 :: (a -> a -> a) -> [a] -> [a] -> [a]
-map2 _ [ ] _ = [ ]
-map2 _ _ [ ] = [ ]
-map2  f  (x:xs) (y:ys) = (f x y) : (map2 f xs ys)
+main = do
+    putStrLn("Введите город вылета:")
+    dep <- getLine
 
-ff2 x y = map2 (\a b -> a + b) x y
+    putStrLn("Введите город прилета:")
+    arr <- getLine
 
-multiplyByN :: Integer -> (Integer -> Integer)
-multiplyByN  n  =  \x -> n * x
-
-add x n = x + n
-flist  ::  [Int  ->  Int]
-flist = map add [1,2,3]
-
-
-addN  ::  Int  ->  [Int]
-addN  n  =  map  (\f  ->  f  n)  flist
-=
+    putStrLn("Перелет из города " ++ dep ++ " в город " ++ arr)
